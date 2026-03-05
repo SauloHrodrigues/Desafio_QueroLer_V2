bd24e5e1b631303c958ceeff1f964ebd40b34917
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2026-03-05 13:46:36 -0300

    Build: Projeto inicial.

---
+++ src/main/java/com/usuario/quero_ler/QueroLerVersao02Application.java
@@ -0,0 +1,13 @@
+package com.usuario.quero_ler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueroLerVersao02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueroLerVersao02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/usuario/quero_ler/QueroLerVersao02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuario.quero_ler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueroLerVersao02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
