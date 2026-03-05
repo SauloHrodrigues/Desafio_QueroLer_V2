22f3b4c25ec2907a4e84f9fda04a640082c2b59b
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2026-03-05 17:10:12 -0300

    feat: Notificacao.

---
+++ src/main/resources/db/migration/V6__criar_tb_notificacao.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tb_notificacao (
+                                id BIGSERIAL PRIMARY KEY,
+                                data_de_criacao TIMESTAMP,
+                                notificacao TEXT NOT NULL
+);
